b0f513ee0b6a5105bcb0c2d1e6bc0ecf56d83e70
Dzung Pham <24899880+dzungvpham@users.noreply.github.com>
2025-08-16 21:07:14 -0700

    Add alumni

--- assets/scripts/people.js
+++ assets/scripts/people.js
@@ -1,55 +1,101 @@
+// IMPORTANT: Whenever you update the student data,
+// make sure to also update the `publications/prepare_pubs.py` file
+// since it's pulling papers based on student names.
 const student_data = [
     {
         "name": "Dzung Pham",
         "link": "https://dzungvpham.github.io/",
         "img": "dzung.jpg",
+        "alumnus": false,
     },
     {
         "name": "Ali Naseh",
         "link": "https://ali7naseh.github.io/",
         "img": "ali.jfif",
+        "alumnus": false,
     },
     {
         "name": "Yuefeng Peng",
         "link": "https://bujuef.github.io/",
         "img": "yuefeng.png",
+        "alumnus": false,
     },
     {
         "name": "Jaechul Roh",
         "link": "https://jrohsc.github.io/",
         "img": "roh.jpg",
+        "alumnus": false,
     },
     {
         "name": "Hyejun Jeong",
         "link": "https://www.linkedin.com/in/june-jeong-311a311a1/",
         "img": "june.jpg",
+        "alumnus": false,
     },
     {
         "name": "Mohammadreza Teymoorianfard",
         "link": "https://www.linkedin.com/in/mohammadreza-teymoorian-fard/",
         "img": "mohammadreza.jpg",
+        "alumnus": false,
     },
     {
         "name": "Arisa Tajima",
         "link": "https://arisa77.github.io/",
         "img": "arisa.jpg",
+        "alumnus": false,
     },
     {
         "name": "Abhinav Kumar",
         "link": "https://www.securegradients.com/",
         "img": "abhinav.jpg",
+        "alumnus": false,
     },
+    {
+        "name": "Alireza Bahramali",
+        "link": "https://www.linkedin.com/in/alireza-bahramali/",
+        "img": "alireza.jpg",
+        "alumnus": true,
+    },
+    {
+        "name": "Virat Shejwalkar",
+        "link": "https://people.cs.umass.edu/~vshejwalkar/",
+        "img": "virat.jpg",
+        "alumnus": true,
+    },
+    {
+        "name": "Hamid Mozaffari",
+        "link": "https://hamidmozaffari.github.io/",
+        "img": "hamid.jpg",
+        "alumnus": true,
+    },
+    {
+        "name": "Milad Nasr",
+        "link": "https://www.linkedin.com/in/milad-nasr-50ab6a56/",
+        "img": "milad.jfif",
+        "alumnus": true,
+    },
+    {
+        "name": "Fatemeh Rezaei",
+        "link": "https://www.linkedin.com/in/fatemerezaei/",
+        "img": "fatemeh.jfif",
+        "alumnus": true,
+    }
 ].sort((a, b) => a.name.localeCompare(b.name));
 
-document.getElementById("students").replaceChildren(...student_data.map(student => {
-    return fromHTML(`<div class="col-4 col-xs-4 col-sm-4 col-md-4 mb-4">
-        <a href="${student.link}" target="_blank" class="card">
-            <div class="card-img-top d-flex justify-content-center align-items-center profile-img-card mb-0">
-                <img src="./assets/images/${student.img}" class="img-fluid profile-img" alt="${student_data.name}">
-            </div>
-            <div class="card-body">
-                <p class="card-title mb-1">${student.name}</p>
-            </div>
-        </a>
-    </div>`);
-}));
+function fillPeopleData(div_id, data) {
+    document.getElementById(div_id).replaceChildren(...data.map(d => {
+        return fromHTML(`<div class="col-4 col-xs-4 col-sm-4 col-md-4 mb-4">
+            <a href="${d.link}" target="_blank" class="card">
+                <div class="card-img-top d-flex justify-content-center align-items-center profile-img-card mb-0">
+                    <img src="./assets/images/${d.img}" class="img-fluid profile-img" alt="${d.name}">
+                </div>
+                <div class="card-body">
+                    <p class="card-title mb-1">${d.name}</p>
+                </div>
+            </a>
+        </div>`);
+    }));
+}
+
+fillPeopleData("students", student_data.filter(s => !s.alumnus));
+fillPeopleData("alumni", student_data.filter(s => s.alumnus));
